47dcf579ac21b6b428add8ff52f47bf2629cccf9
Athirapr-1994 <athirapr.ravi@gmail.com>
2025-07-17 13:43:45 +0000

    fuelphp

---
+++ my_project_name/app/fuel/logs/2025/07/17.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2025-07-17 13:42:14 --> Fatal Error - The configured locale(s) "en_US" can not be found on your system. in /workspaces/sample/my_project_name/fuel/core/classes/fuel.php on line 182
